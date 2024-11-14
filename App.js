07cfa2714a730c5d64ab1a52493476ed0b05246a
psodm <peter.stacey@broadcom.com>
2024-11-14 20:52:50 +1100

    inital commit

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.dummyText}>Some other text!</Text>
+      </View>
+      <Text style={styles.dummyText}>Hello World!</Text>
+      <Button title='Tap me!'/>
     </View>
   );
 }
@@ -17,4 +20,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dummyText: {
+    margin: 20, 
+    padding: 10, 
+    borderWidth: 2, 
+    borderColor: 'green'
+  }
 });
